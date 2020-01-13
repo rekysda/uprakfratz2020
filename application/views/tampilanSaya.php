c0ea1cb3822bc656009b8a572be5f76e3a00fbb0
rekysda <rekysda@gmail.com>
2020-01-13 09:26:28 +0700

    20200113

---
+++ application/views/tampilanSaya.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>CI Trial Controllers & Views</title>
+</head>
+
+<body><br>
+
+	<center>
+	<iframe src="http://cubeindo.com/caritourtask" frameborder="0" scrolling="no" height="500" style="width: 100%;"></iframe>
+	</center>
+	
+</body>
+</html>
